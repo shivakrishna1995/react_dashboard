55861d818ea6801c14375e28cadb3afc55ba25fc
krish <gopannagarishiva@gmail.com>
2020-02-19 23:19:45 +0530

    First commit

---
+++ precache-manifest.e8f513b5491cede3917e80e887220901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b998312ef3111807894be9746fc87fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bbaa9709f4f76228d44",
+    "url": "/static/css/main.0ce06907.chunk.css"
+  },
+  {
+    "revision": "d1fdd8399dc642fbfa14",
+    "url": "/static/js/2.6cac85f5.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.6cac85f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbaa9709f4f76228d44",
+    "url": "/static/js/main.02084fc8.chunk.js"
+  },
+  {
+    "revision": "19a4885259fe2d34d6b6",
+    "url": "/static/js/runtime-main.e116b58e.js"
+  }
+]);
